06ae617ad790339511e5ef91e2f66714ef8f6a95
Coleton Wilczynski <cnwilczy@mtu.edu>
2025-11-06 19:33:07 -0500

    Update testProcedures.sql

--- testProcedures.sql
+++ testProcedures.sql
@@ -1,6 +1,18 @@
-select * from cart natural join cart_item where c_id = 2;
-select * from orders natural join order_item where o_id = 11;
-CALL checkout(2, @o_id, @error_product);
-select * from cart natural join cart_item where c_id = 2;
-select * from orders natural join order_item where o_id = @o_id;
-select @error_product
+use jopking;
+
+-- select * from cart natural join cart_item where c_id = 2;
+-- select * from orders natural join order_item where o_id = 11;
+-- CALL checkout(2, @o_id, @error_product);
+-- select * from cart natural join cart_item where c_id = 2;
+-- select * from orders natural join order_item where o_id = @o_id;
+-- select @error_product;
+
+-- DELETE FROM product
+-- WHERE p_id = 1;
+
+-- UPDATE product
+-- SET p_id = '91398'
+-- WHERE p_id = '1';
+
+
+
